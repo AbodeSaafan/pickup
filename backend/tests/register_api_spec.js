19a875653128b2917f9c2c6411e4f5d4fa5480fc
Abdulqader Saafan <abdulqader.saafan@mail.utoronto.ca>
2017-07-25 13:08:23 -0400

    Added test case

--- backend/tests/register_api_spec.js
+++ backend/tests/register_api_spec.js
@@ -1,8 +1,11 @@
 var frisby = require('frisby');
 var crypto = require('crypto');
+var request = require('request');
 
+var registerEndpoint = 'http://localhost:3000/api/register';
+// Testing creating a user with valid creds
 frisby.create('Register a user using the API')
-  .post('http://localhost:3000/api/register', {
+  .post(registerEndpoint, {
     nickname:'abode',
     password:'password123',
     fname:'abode',
@@ -16,6 +19,27 @@ frisby.create('Register a user using the API')
   .expectBodyContains('token')
   .expectBodyContains('user_id')
 .toss();
+
+// Testing creating a user with an invalid email
+var genericUser = {
+    nickname:'abode',
+    password:'password123',
+    fname:'abode',
+    lname:'saafan',
+    gender:'m',
+    dob:'25/03/1996',
+    email:'abode@mail.com'
+  };
+
+request.post(registerEndpoint, genericUser);
+
+frisby.create('Register a user using the API')
+  .post(registerEndpoint, genericUser)
+  .expectStatus(400)
+  .expectHeaderContains('content-type', 'application/json')
+  .expectBodyContains('error')
+.toss();
+
 
 function randomEmail(){
   return crypto.randomBytes(4).toString('hex') + "@mail.com";
