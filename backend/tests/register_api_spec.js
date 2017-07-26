eddb6ff43cfa905b9d535377c4a4d7dbea48a31e
Abdulqader Saafan <abdulqader.saafan@mail.utoronto.ca>
2017-07-26 10:55:17 -0400

    Added another test case

--- backend/tests/register_api_spec.js
+++ backend/tests/register_api_spec.js
@@ -4,8 +4,9 @@ var request = require('request');
 var strings = require('../api/universal_strings');
 
 var registerEndpoint = 'http://localhost:3000/api/register';
-// Testing creating a user with valid creds
-frisby.create('Register a user using the API')
+
+// Creating a user with valid creds
+frisby.create('Register a user using the API with valid credentials')
   .post(registerEndpoint, {
     nickname:'abode',
     password:'password123',
@@ -21,7 +22,25 @@ frisby.create('Register a user using the API')
   .expectBodyContains('user_id')
 .toss();
 
-// Testing creating a user with an invalid email
+// Sending a register request without some params
+frisby.create('Attempt to register a user using the API with missing parameters')
+  .post(registerEndpoint, {
+    password:'password123',
+    fname:'abode',
+    lname:'saafan',
+    gender:'m',
+    dob:'25/03/1996',
+    email:'abode@mail.com'
+  }) 
+  .expectStatus(400)
+  .expectHeaderContains('content-type', 'application/json')
+  .expectJSON({
+    error: strings.invalidNickname
+  })
+  
+.toss();
+
+// Creating a user with an invalid email
 var genericUser = {
     nickname:'abode',
     password:'password123',
@@ -34,7 +53,7 @@ var genericUser = {
 
 request.post(registerEndpoint, genericUser);
 
-frisby.create('Register a user using the API')
+frisby.create('Register a user using the API with an invalid email')
   .post(registerEndpoint, genericUser)
   .expectStatus(400)
   .expectHeaderContains('content-type', 'application/json')
