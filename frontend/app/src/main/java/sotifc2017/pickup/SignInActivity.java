3fbebefd6ae42b4c340fa73f889e2b478f5a6160
Quinn Daneyko <quinn.daneyko@mail.utoronto.ca>
2017-08-10 00:04:15 -0400

    Progress in SignUp.
    Fixed Manifest, so app shouldn't crash immediately on load.

--- frontend/app/src/main/java/sotifc2017/pickup/SignInActivity.java
+++ frontend/app/src/main/java/sotifc2017/pickup/SignInActivity.java
@@ -2,6 +2,8 @@ package sotifc2017.pickup;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+    }
+
+    public void newAccount(View view) {
+        Toast.makeText(this, "New Account...", Toast.LENGTH_SHORT).show();
+    }
+
+    public void signIn(View view) {
+        Toast.makeText(this, "Signing in...", Toast.LENGTH_SHORT).show();
     }
 }
