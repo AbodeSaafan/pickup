1f4a7d924c76b8eb658a094ab5b3913fba37d28b
radhika2896 <radhika.krishnan@mail.utoronto.ca>
2017-08-22 22:58:05 -0400

    added check for invalid userID when update

--- backend/api/extended_profile.js
+++ backend/api/extended_profile.js
@@ -38,15 +38,24 @@ router.put('/:user_id', function (req, res) {
   var location = req.query.location;
   //tokenHelper.verifyToken(req.headers.token);
   try {
-    databaseHelper.updateExtendedUser(userID, skill_level, location, (update) => {
-      if (update) {
-        console.log('update was successful');
-        res.status(200).json(update);
-      } else {
-        console.log(update)
-        res.status(400).json({'error': strings.userIdFail});
-        return;
-      }
+    databaseHelper.getExtendedProfile(userID, (user_id) => {
+        if(user_id) {
+            console.log(user_id);
+            databaseHelper.updateExtendedUser(userID, skill_level, location, (update) => {
+              if (update) {
+                console.log('update was successful');
+                res.status(200).json(update);
+              } else {
+                console.log(update)
+                res.status(400).json({'error': strings.userIdFail});
+                return;
+              }
+            })
+            return;
+        }else{
+            res.status(400).json({'error': strings.userIdFail});
+            return;
+        }
     })
   } catch (err) {
     res.status(400).json({'error': strings.invalidJwt});
