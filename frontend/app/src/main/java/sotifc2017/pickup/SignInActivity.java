f8de01c2f75ee46fea5a7f02f69e2f54885dcd37
Quinn Daneyko <quinn.daneyko@mail.utoronto.ca>
2017-08-30 00:32:58 -0400

    Progress in SignIn. More progress on spinner and design of SignIn.

--- frontend/app/src/main/java/sotifc2017/pickup/SignInActivity.java
+++ frontend/app/src/main/java/sotifc2017/pickup/SignInActivity.java
@@ -1,12 +1,21 @@
 package sotifc2017.pickup;
 
+import android.app.ActionBar;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -14,6 +23,11 @@ public class SignInActivity extends AppCompatActivity {
     private String email;
     private EditText passText;
     private String pass;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +36,9 @@ public class SignInActivity extends AppCompatActivity {
 
         emailText = (EditText) findViewById(R.id.emailEditText);
         passText = (EditText) findViewById(R.id.passEditText);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     public void newAccount(View view) {
@@ -36,20 +53,22 @@ public class SignInActivity extends AppCompatActivity {
                 R.style.AppTheme_Dark);
         progressDialog.setIndeterminate(true);
         progressDialog.setMessage("Authenticating...");
+
+        Window window = progressDialog.getWindow();
+        window.setLayout(ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT);
         progressDialog.show();
 
         email = emailText.getText().toString();
         pass = passText.getText().toString();
 
-        new android.os.Handler().postDelayed(
+        new Handler().postDelayed(
                 new Runnable() {
                     public void run() {
                         // On complete call either onLoginSuccess or onLoginFailed
                         if (authenticateUser(email, pass)) {
                             progressDialog.dismiss();
                             signInSuccess();
-                        }
-                        else {
+                        } else {
                             progressDialog.dismiss();
                             signInFailure();
                         }
@@ -72,5 +91,41 @@ public class SignInActivity extends AppCompatActivity {
 
     private boolean authenticateUser(String email, String password) {
         return false;
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("SignIn Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
