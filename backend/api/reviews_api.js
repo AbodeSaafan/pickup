4b8402b6392a47d84a1e8c24fcf4329fdf9c2b36
Pavle Arezina <pavle.arezina@soti.net>
2017-09-05 15:35:49 -0400

    move api extension to different endpoint

--- backend/api/reviews_api.js
+++ backend/api/reviews_api.js
@@ -3,50 +3,6 @@ var router = express.Router();
 var requestHelper = require('../helpers/requestHelper');
 var databaseHelper = require('../helpers/databaseHelper');
 
-/**
- * @api {get} /reviews/:gameid Get users of a game
- * @apiName Get game
- * @apiGroup Games
- *
- * @apiParam {int} id of the game
- *
- * @apiError error The error field has a string with an exact error
- *
- * @apiSuccessExample Success-Response:
- *      HTTP/1.1 200 OK
- *     {
-*       "email": "6209be52@mail.com",
-*       "password": "fa2568a8dd82c24a6ee22df3f19d642d"
-*     }
- *
- * @apiSampleRequest /api/reviews/:123
- */
-router.get('/:game_id', function(req, res){
-	var gameid = req.params.game_id;
-	try {
-
-      	tokenHelper.verifyToken(req.headers.token);
-      	databaseHelper.getUsers(gameid , (user_id) => {
-          if(user_id) {
-              console.log(user_id);
-              res.status(200).json(user_id);
-              return;
-          }else{
-              res.status(400).json({'error': strings.userIdFail});
-              return;
-          }
-        })
-	 }
-	 catch(err){
-
-       res.status(400).json({'error': strings.invalidJwt});
-       return;
-    }
-    
-
-
-	});
-
 
 router.post('/setReview', function(req, res){
 	try {
