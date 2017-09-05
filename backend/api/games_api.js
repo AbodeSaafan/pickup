8879336f255da8b7225f2b23a10f806b593a1c61
Pavle Arezina <pavle.arezina@soti.net>
2017-09-05 15:35:25 -0400

    move api extension to different endpoint

--- backend/api/games_api.js
+++ backend/api/games_api.js
@@ -58,4 +58,47 @@ router.post('/', function(req, res){
 	}
 });
 
+/**
+ * @api {get} /games/:gameid Get users of a game
+ * @apiName Get game
+ * @apiGroup Games
+ *
+ * @apiParam {int} id of the game
+ *
+ * @apiError error The error field has a string with an exact error
+ *
+ * @apiSuccessExample Success-Response:
+ *      HTTP/1.1 200 OK
+ *     {
+*       
+*     }
+ *
+ * @apiSampleRequest /api/games/:123
+ */
+router.get('/:game_id', function(req, res){
+	var gameid = req.params.game_id;
+	try {
+
+      	tokenHelper.verifyToken(req.headers.token);
+      	databaseHelper.getUsers(gameid , (user_id) => {
+          if(user_id) {
+              console.log(user_id);
+              res.status(200).json(user_id);
+              return;
+          }else{
+              res.status(400).json({'error': strings.userIdFail});
+              return;
+          }
+        })
+	 }
+	 catch(err){
+
+       res.status(400).json({'error': strings.invalidJwt});
+       return;
+    }
+    
+
+
+	});
+
 module.exports = router;
