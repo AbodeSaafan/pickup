8984a2bdb12ac11edac69a29541356d0e9a41da6
Abdulqader Saafan <abdulqader.saafan@mail.utoronto.ca>
2017-09-06 12:01:26 -0400

    Updating db table scripts, adding line that allows you to drop data in table so you can recreate with new scripts

--- backend/database_scripts/games_table.sql
+++ backend/database_scripts/games_table.sql
@@ -2,12 +2,16 @@ CREATE TABLE IF NOT EXISTS Game (
 	game_id BIGSERIAL PRIMARY KEY,
 	name varchar(255),
 	type varchar(255),
-	intended_skill int,
+	skill int,
 	total_players_required int,
-	start_time timestamptz,
-	duration time,
-	location varchar(255),
+	start_time int,
+	duration int,
+	location point,
 	creator_id int REFERENCES Users(user_id),
 	description varchar(255),
-	time_created timestamptz
+	location_notes varchar(255),
+	gender varchar(255),
+	age_range int[2],
+	enforced_params varchar(255)[3],
+	time_created int DEFAULT extract(epoch from now())
 );
--- backend/database_scripts/runAllScripts.cmd
+++ backend/database_scripts/runAllScripts.cmd
@@ -1,1 +1,2 @@
+::psql -h localhost -p 5432 -U postgres -f dropAll.sql pickup
 psql -h localhost -p 5432 -U postgres -f createAll.sql pickup
