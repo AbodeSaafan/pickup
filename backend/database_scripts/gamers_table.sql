4f48f7490635006fc6a11e94fed6ba82f91afdaa
Abdulqader Saafan <abdulqader.saafan@mail.utoronto.ca>
2017-09-11 16:40:37 -0400

    Adding time_joined default field to gamers table

--- backend/database_scripts/gamers_table.sql
+++ backend/database_scripts/gamers_table.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS gamers (
 	user_id int REFERENCES Users(user_id),
 	game_id int REFERENCES Games(game_id),
+	time_joined int DEFAULT extract(epoch from now()),
 	PRIMARY KEY(user_id, game_id)
 );
