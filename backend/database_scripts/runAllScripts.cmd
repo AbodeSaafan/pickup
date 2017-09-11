762fa366e68026427fcd208bf46675e5e15b7dda
Abdulqader Saafan <abdulqader.saafan@mail.utoronto.ca>
2017-09-11 16:59:17 -0400

    making script better, just run it, no need to enter password

--- backend/database_scripts/runAllScripts.cmd
+++ backend/database_scripts/runAllScripts.cmd
@@ -1,2 +1,3 @@
-psql -h localhost -p 5432 -U postgres -f dropAll.sql pickup
+set PGPASSWORD=123
+psql -h localhost -p 5432 -U postgres -f dropAll.sql pickup 
 psql -h localhost -p 5432 -U postgres -f createAll.sql pickup
