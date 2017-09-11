1f4cb54d04e3fa6452bf58c64c95d56f509d53e0
Abdulqader Saafan <abdulqader.saafan@mail.utoronto.ca>
2017-09-11 16:39:22 -0400

    Updating query to cover case where creator attempts to create a game at a time which they are playing another game (joined another game)

--- backend/helpers/databaseHelper.js
+++ backend/helpers/databaseHelper.js
@@ -244,10 +244,13 @@ function createGame (userId, name, type, skill, totalPlayers, startTime, duratio
 }
 
 function ensureGameIsValid (game, userId, callback){
-	// TODO ensure that the user is not joined in any conflicting games either
-	var queryString = "SELECT start_time, end_time FROM games WHERE creator_id = $1 AND (($2 >= start_time AND $2 <= end_time) OR ($3 >= start_time AND $3 <= end_time) OR (start_time >= $2 AND start_time <= $3) OR (end_time >= $2 AND end_time <= $3)) ORDER BY start_time ASC";
+	var queryString = "SELECT start_time, end_time " + 
+		"FROM (games FULL OUTER JOIN gamers ON games.game_id=gamers.game_id) " +
+		"WHERE (creator_id = $1 OR user_id = $1) AND (($2 >= start_time AND $2 <= end_time) OR ($3 >= start_time AND $3 <= end_time) OR (start_time >= $2 AND start_time <= $3) OR (end_time >= $2 AND end_time <= $3))";
 	var end_time = game.start_time + game.duration;
 	var queryParams = [userId, game.start_time, end_time];
+
+	console.log(queryParams);
 	
 	const pool = new pg.Pool({connectionString: conString});
 
