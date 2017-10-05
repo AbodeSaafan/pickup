cedd0b99b60c2299799f448d967ea780480801cc
radhika2896 <radhika.krishnan@mail.utoronto.ca>
2017-10-04 22:29:12 -0400

    fixed extendedProfileTests

--- backend/api/extended_profile.js
+++ backend/api/extended_profile.js
@@ -11,52 +11,66 @@ var strings = require('./universal_strings');
 // endpoint should be -> /api/extendedProfile/:user_id
 //api to get extended profile
 
-router.get('/:user_id', function (req, res) {
-  var userID = req.params.user_id;
-  try {
-      databaseHelper.getExtendedProfile(userID, (user_id) => {
-          if(user_id) {
-              res.status(200).json(user_id);
-              return;
-          }else{
-              res.status(400).json({'error': strings.userIdFail});
-              return;
-          }
-        })
-    }catch(err){
-       res.status(400).json({'error': strings.invalidJwt});
-       return;
-    }
+router.get('/', function (req, res) {
+
+  var token = req.query.jwt;
+
+  tokenHelper.verifyToken(token);
+
+  try{
+    var userId = tokenHelper.getUserFromToken(token).user_id;
+  }
+  catch(err){
+    res.status(400).json(requestHelper.jsonError(err)); return;
+  }
+
+  databaseHelper.getExtendedProfile(userId, (user_id) => {
+      if(user_id) {
+          console.log(user_id);
+          res.status(200).json(user_id);
+          return;
+      }else{
+          res.status(400).json({'error': strings.userIdFail});
+          return;
+      }
+    })
 });
 
 // endpoint should be -> /api/extendedProfile/:user_id?skill_level=&location=
 //api to update extended profile (skill_level)
-router.put('/:user_id', function (req, res) {
-  var userID = req.params.user_id;
+router.put('/', function (req, res) {
+
+  var token = req.query.jwt;
+
+  tokenHelper.verifyToken(token);
+
+  try{
+    var userId = tokenHelper.getUserFromToken(token).user_id;
+  }
+  catch(err){
+    res.status(400).json(requestHelper.jsonError(err)); return;
+  }
+
   var skill_level = req.query.skill_level;
   var location = req.query.location;
-  tokenHelper.verifyToken(req.headers.token);
-  try {
-    databaseHelper.getExtendedProfile(userID, (user_id) => {
-        if(user_id) {
-            databaseHelper.updateExtendedUser(userID, skill_level, location, (update) => {
-              if (update) {
-                res.status(200).json(update);
-              } else {
-                res.status(400).json({'error': strings.userIdFail});
-                return;
-              }
-            })
-            return;
-        }else{
-            res.status(400).json({'error': strings.userIdFail});
-            return;
-        }
-    })
-  } catch (err) {
-    res.status(400).json({'error': strings.invalidJwt});
-    return;
-  }
+
+  databaseHelper.getExtendedProfile(userId, (user_id) => {
+      if(user_id) {
+          databaseHelper.updateExtendedUser(userId, skill_level, location, (update) => {
+            if (update) {
+              console.log(update)
+              res.status(200).json(update);
+            } else {
+              res.status(400).json({'error': strings.userIdFail});
+              return;
+            }
+          })
+          return;
+      }else{
+          res.status(400).json({'error': strings.userIdFail});
+          return;
+      }
+  })
 });
 
 module.exports = router;
