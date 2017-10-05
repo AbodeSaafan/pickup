577f32bca5ebd97a9fafeca5a696944117a106c4
Nio Peng <nio.peng@soti.net>
2017-10-05 17:06:41 -0400

    Adding profile self view

---
+++ frontend/app/src/main/java/sotifc2017/pickup/ProfileSelfActivity.java
@@ -0,0 +1,13 @@
+package sotifc2017.pickup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProfileSelfActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_self);
+    }
+}
--- frontend/app/src/main/java/sotifc2017/pickup/SignUpActivity.java
+++ frontend/app/src/main/java/sotifc2017/pickup/SignUpActivity.java
@@ -3,6 +3,7 @@ package sotifc2017.pickup;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -282,6 +283,9 @@ public class SignUpActivity extends AppCompatActivity implements LoaderCallbacks
             mAuthTask = new UserLoginTask(email, firstname, lastname, password, gender, username);
             mAuthTask.execute((Void) null);
         }
+
+        Intent intent = new Intent(this, ProfileSelfActivity.class);
+        startActivity(intent);
     }
 
     private boolean isEmailValid(String email) {
@@ -442,7 +446,8 @@ public class SignUpActivity extends AppCompatActivity implements LoaderCallbacks
             showProgress(false);
 
             if (success) {
-                finish();
+
+                //finish();
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();
