3e9041616ef7fb67591a6bd045479ce5352a3570
Nio Peng <nio.peng@soti.net>
2017-12-04 14:25:23 -0500

    Adding register response object #76

---
+++ frontend/app/src/main/java/sotifc2017/pickup/api/contracts/RegisterResponse.java
@@ -0,0 +1,11 @@
+package sotifc2017.pickup.api.contracts;
+
+/**
+ * Created by npeng on 12/4/2017.
+ */
+
+public class RegisterResponse {
+        public String refresh;
+        public int user_id;
+        public String token;
+}
