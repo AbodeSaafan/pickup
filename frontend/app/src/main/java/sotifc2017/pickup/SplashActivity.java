f8a196c4c11d097ec9a9790c994f11463d8f6d6b
Abdulqader Saafan <abdulqader.saafan@mail.utoronto.ca>
2018-02-04 01:54:41 -0500

    Added progress bar and temp logo on splash screen #87

--- frontend/app/src/main/java/sotifc2017/pickup/SplashActivity.java
+++ frontend/app/src/main/java/sotifc2017/pickup/SplashActivity.java
@@ -3,6 +3,11 @@ package sotifc2017.pickup;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import sotifc2017.pickup.api.GetJwt;
 
@@ -12,9 +17,9 @@ public class SplashActivity extends AppCompatActivity implements GetJwt.Callback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.activity_splash);
 
         new GetJwt(this).execute(this);
-        setContentView(R.layout.activity_splash);
     }
 
     @Override
