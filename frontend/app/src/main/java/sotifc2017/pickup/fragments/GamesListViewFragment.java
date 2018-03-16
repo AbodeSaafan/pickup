55a6c78a25717cf1e9fb01c06651246d97cea5c7
Abdulqader Saafan <abdulqader.saafan@mail.utoronto.ca>
2018-03-16 11:22:10 -0400

    Re-adding deleted file and changing package to lowercase for ocd #119

--- frontend/app/src/main/java/sotifc2017/pickup/fragments/GamesListViewFragment.java
+++ frontend/app/src/main/java/sotifc2017/pickup/fragments/GamesListViewFragment.java
@@ -21,7 +21,7 @@ import sotifc2017.pickup.activities.SignInActivity;
 import sotifc2017.pickup.api.Authentication;
 import sotifc2017.pickup.api.GetJwt;
 import sotifc2017.pickup.api.models.GameModel;
-import sotifc2017.pickup.Adapters.GameListAdapter;
+import sotifc2017.pickup.adapters.GameListAdapter;
 
 /**
  * Created by radhika on 2018-03-10.
