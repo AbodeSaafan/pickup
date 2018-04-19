dd3575515fe9309800cb57b75dc025a528832c0e
Abdulqader Saafan <abdulqader.saafan@mail.utoronto.ca>
2018-04-19 17:34:31 -0400

    #124: Adding the skill level range bar for search

--- frontend/app/src/main/java/sotifc2017/pickup/fragments/SearchGamesFragment.java
+++ frontend/app/src/main/java/sotifc2017/pickup/fragments/SearchGamesFragment.java
@@ -7,9 +7,13 @@ import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import com.appyvet.materialrangebar.RangeBar;
 
 import org.adw.library.widgets.discreteseekbar.DiscreteSeekBar;
 
@@ -24,8 +28,13 @@ import sotifc2017.pickup.R;
 public class SearchGamesFragment extends Fragment {
     ImageButton toggleButton;
     RelativeLayout childSection;
-    DiscreteSeekBar minPlayerSeekBar;
+
+    CheckBox casualGameCheck;
+    CheckBox seriousGameCheck;
+    RangeBar minPlayerSeekBar;
     TextView minPlayerText;
+    TextView skillRangeText;
+    RangeBar skillRange;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -36,8 +45,17 @@ public class SearchGamesFragment extends Fragment {
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        casualGameCheck = view.findViewById(R.id.casualGameTypeCheckBox);
+        seriousGameCheck = view.findViewById(R.id.seriousGameTypeCheckBox);
+
         minPlayerSeekBar = view.findViewById(R.id.min_players_seekbar);
         minPlayerText = view.findViewById(R.id.minimum_players_label);
+        minPlayerText.setText(String.format(getString(R.string.game_search_minimum_player_message), 20));
+
+        skillRange = view.findViewById(R.id.skill_seekBar);
+        skillRangeText = view.findViewById(R.id.skill_level_label);
+        skillRangeText.setText(String.format(getString(R.string.game_search_skill_range), 1, 10));
+
 
         // Will generalize later
         toggleButton = view.findViewById(R.id.filters_header_toggle);
@@ -68,17 +86,31 @@ public class SearchGamesFragment extends Fragment {
         });
         // Will generalize later
 
-        minPlayerSeekBar.setOnProgressChangeListener(new DiscreteSeekBar.OnProgressChangeListener() {
+        seriousGameCheck.setOnCheckedChangeListener(new CheckBox.OnCheckedChangeListener() {
             @Override
-            public void onProgressChanged(DiscreteSeekBar seekBar, int value, boolean fromUser) {
-                minPlayerText.setText(String.format(Locale.CANADA, getString(R.string.game_search_minimum_player_message), value));
+            public void onCheckedChanged(CompoundButton compoundButton, boolean seriousGameChecked) {
+                skillRange.setEnabled(seriousGameChecked);
+                /*if(!seriousGameChecked){ // Uncomment this section if we want to reset the skill range when serious game is disabled
+                    skillRange.setRangePinsByIndices(0, skillRange.getTickCount() - 1);
+                }*/
+            }
+        });
+
+        minPlayerSeekBar.setOnRangeBarChangeListener(new RangeBar.OnRangeBarChangeListener() {
+            @Override
+            public void onRangeChangeListener(RangeBar rangeBar, int leftPinIndex,
+                                              int rightPinIndex, String leftPinValue, String rightPinValue) {
+                minPlayerText.setText(String.format(getString(R.string.game_search_minimum_player_message), Integer.parseInt(rightPinValue)));
+            }
+        });
+
+        skillRange.setOnRangeBarChangeListener(new RangeBar.OnRangeBarChangeListener() {
+            @Override
+            public void onRangeChangeListener(RangeBar rangeBar, int leftPinIndex,
+                                              int rightPinIndex, String leftPinValue, String rightPinValue) {
+                skillRangeText.setText(String.format(getString(R.string.game_search_skill_range), Integer.parseInt(leftPinValue), Integer.parseInt(rightPinValue)));
             }
 
-            @Override
-            public void onStartTrackingTouch(DiscreteSeekBar seekBar) {}
-
-            @Override
-            public void onStopTrackingTouch(DiscreteSeekBar seekBar) {}
         });
 
         super.onViewCreated(view, savedInstanceState);
