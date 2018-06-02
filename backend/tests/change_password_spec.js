1482399dcc01f03dadf5482411ca945c99e3b728
Abdulqader Saafan <abdulqader.saafan@mail.utoronto.ca>
2018-06-02 19:34:16 -0400

    #162: Converted change password tests

--- backend/tests/change_password_spec.js
+++ backend/tests/change_password_spec.js
@@ -2,78 +2,51 @@ var frisby = require("frisby");
 var testHelper = require("./testHelper");
 var strings = require("../api/universal_strings");
 
-// Provide a valid update password request for a valid user
-// Check if user can log in with new Password
-// Check if user cannot log in with old Password
-
-var user = testHelper.createGenericUserMale();
-var new_password = testHelper.randomPassword();
-frisby.create("Register a user using the API with valid credentials to use for profile testing")
-	.post(testHelper.registerEndpoint, user)
-	.expectStatus(200)
-	.afterJSON(function (body) {
-		frisby.create("Update User password")
-			.put(testHelper.changePasswordEndpoint, {jwt: body.token, old_password: user.password, new_password: new_password})
-			.expectStatus(200)
-			.afterJSON(function() {
-				frisby.create("Log the user in with new password")
-					.post(testHelper.loginEndpoint, {email: user.email, password: new_password})
-					.expectStatus(200)
-					.expectBodyContains("token")
-					.expectBodyContains("user_id")
-					.afterJSON(function() {
-						frisby.create("Log the user in with old password")
-							.post(testHelper.loginEndpoint, {email: user.email, password: user.password})
-							.expectStatus(400)
-							.expectJSON({
-								error: strings.loginError
-							})
-							.toss();
+describe("Testing change password api", function () {
+	it("Can change a user's password, making the old password invalid", function() {
+		var user = testHelper.createGenericUserMale();
+		var new_password = testHelper.randomPassword();
+		return frisby.post(testHelper.registerEndpoint, user)
+			.expect("status", 200)
+			.then(function (body) {
+				body = body.json;
+				return frisby.put(testHelper.changePasswordEndpoint, {jwt: body.token, old_password: user.password, new_password: new_password})
+					.expect("status", 200)
+					.then(function() {
+						return frisby.post(testHelper.loginEndpoint, {email: user.email, password: new_password})
+							.expect("status", 200)
+							.expect("bodyContains", "token")
+							.expect("bodyContains", "user_id")
+							.then(function() {
+								return frisby.post(testHelper.loginEndpoint, {email: user.email, password: user.password})
+									.expect("status", 400)
+									.expect("jsonStrict", {
+										error: strings.loginError
+									});
+							});
+					});
+			});
+	});
+	
+	
+	it("User should not be able to change their password with an invalid password passed in", function() {
+		var user = testHelper.createGenericUserMale();
+		var new_user = testHelper.createGenericUserMale();
+		return frisby.post(testHelper.registerEndpoint, user)
+			.expect("status", 200)
+			.then(function (body) {
+				body = body.json;
+				return frisby.put(testHelper.changePasswordEndpoint, {jwt: body.token, old_password: new_user.password, new_password: "/"})
+					.expect("status", 400)
+					.expect("jsonStrict", {
+						error: strings.invalidPassword
 					})
-					.toss();
-			})
-			.toss();
-	})
-	.toss();
-
-
-//Check if error is prompted if user logs in with incorrect old Password
-
-frisby.create("Register a user using the API with valid credentials to use for profile testing")
-	.post(testHelper.registerEndpoint, testHelper.createGenericUserMale())
-	.expectStatus(200)
-	.afterJSON(function (body) {
-		frisby.create("Update User password")
-			.put(testHelper.changePasswordEndpoint, {jwt: body.token, old_password: "1234", new_password: new_password})
-			.expectStatus(400)
-			.expectJSON({
-				error: strings.invalidPassword
-			})
-			.toss();
-	})
-	.toss();
-
-
-
-//Check if error is prompted if user enters an invalid new password
-var new_user = testHelper.createGenericUserMale();
-frisby.create("Register a user using the API with valid credentials to use for profile testing")
-	.post(testHelper.registerEndpoint, testHelper.createGenericUserMale())
-	.expectStatus(200)
-	.afterJSON(function (body) {
-		frisby.create("Update User password")
-			.put(testHelper.changePasswordEndpoint, {jwt: body.token, old_password: new_user.password, new_password: "/"})
-			.expectStatus(400)
-			.expectJSON({
-				error: strings.invalidPassword
-			})
-			.afterJSON(function () {
-				frisby.create("check login with old password")
-					.post(testHelper.loginEndpoint, {email: user.email, password: new_user.password})
-					.expectStatus(200)
-					.expectBodyContains("token")
-					.expectBodyContains("user_id");
-			})
-			.toss();
-	})
-	.toss();
+					.then(function () {
+						return frisby.post(testHelper.loginEndpoint, {email: user.email, password: user.password})
+							.expect("status", 200)
+							.expect("bodyContains", "token")
+							.expect("bodyContains", "user_id");
+					});
+			});
+	});
+});
