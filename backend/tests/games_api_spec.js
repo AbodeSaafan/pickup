d7f214e422caa8e8190e2fbc9a78fbb2ab4afe30
Abdulqader Saafan <abdulqader.saafan@mail.utoronto.ca>
2018-06-03 17:01:30 -0400

    #162: Converted games testing, fixed minor issue with converter

--- backend/tests/games_api_spec.js
+++ backend/tests/games_api_spec.js
@@ -3,268 +3,218 @@ var strings = require("../api/universal_strings");
 var testHelper = require("./testHelper");
 const util = require("util");
 
-// Creating a unrestricted game
-frisby.create("Register a user using the API with valid credentials to use for creating a game")
-	.post(testHelper.registerEndpoint, testHelper.createGenericUserMale()) 
-	.expectStatus(200)
-	.expectHeaderContains("content-type", "application/json")
-	.expectBodyContains("token")
-	.expectBodyContains("user_id")
-	.expectBodyContains("refresh")
-	.afterJSON(function (body) {
-		frisby.create("Creating a new game")
-			.post(testHelper.createGameEndpoint, testHelper.createUnrestrictedGame(body.token, 1, 1))
-			.expectStatus(200)
-			.expectBodyContains("game_id")
-			.toss();
-	})
-	.toss();
+describe("Games api testing", function() {
+	it("Should be able to create a game with no restrictions", function() {
+		return frisby.post(testHelper.registerEndpoint, testHelper.createGenericUserMale()) 
+			.expect("status", 200)
+			.expect("bodyContains", "token")
+			.expect("bodyContains", "user_id")
+			.expect("bodyContains", "refresh")
+			.then(function (body) {
+				body = body.json;
+				return frisby.post(testHelper.createGameEndpoint, testHelper.createUnrestrictedGame(body.token, 1, 1))
+					.expect("status", 200)
+					.expect("bodyContains", "game_id");
+			});
+	});
 
-// Creating a game with enforced params
-frisby.create("Register a user using the API with valid credentials to use for creating a game with enforced params")
-	.post(testHelper.registerEndpoint, testHelper.createGenericUserMale())
-	.expectStatus(200)
-	.expectBodyContains("token")
-	.afterJSON(function (body) {
-		frisby.create("Creating a new game")
-			.post(testHelper.createGameEndpoint, testHelper.createGenericGame(body.token, 1, 1))
-			.expectStatus(200)
-			.expectBodyContains("game_id")
-			.toss();
-	})
-	.toss();
+	it("Should be able to create a game with some enforced parameters", function() {
+		return frisby.post(testHelper.registerEndpoint, testHelper.createGenericUserMale())
+			.expect("status", 200)
+			.expect("bodyContains", "token")
+			.then(function (body) {
+				body = body.json;
+				return frisby.post(testHelper.createGameEndpoint, testHelper.createGenericGame(body.token, 1, 1))
+					.expect("status", 200)
+					.expect("bodyContains", "game_id");
+			});
+	});
 
-/*
-* Create game API tests
-* */
-
-// Using a bad token to create game should fail it
-frisby.create("Creating a new game")
-	.post(testHelper.createGameEndpoint, testHelper.createUnrestrictedGame({user_id: "1", email: "ab@mail.com"}))
-	.expectStatus(400)
-	.toss();
+	it("Should not allow a user to create a game with an invalid jwt token", function() {
+		return frisby.post(testHelper.createGameEndpoint, testHelper.createUnrestrictedGame({user_id: "1", email: "ab@mail.com"}))
+			.expect("status", 400);
+	});
 
 
-// Creating a game that conflicts with another game you have created should fail
-frisby.create("Register a user using the API with valid credentials to use for creating a game")
-	.post(testHelper.registerEndpoint, testHelper.createGenericUserMale()) 
-	.expectStatus(200)
-	.expectHeaderContains("content-type", "application/json")
-	.expectBodyContains("token")
-	.expectBodyContains("user_id")
-	.expectBodyContains("refresh")
-	.afterJSON(function (body) {
-		frisby.create("Creating a new game")
-			.post(testHelper.createGameEndpoint, testHelper.createUnrestrictedGame(body.token, 100, 200))
-			.expectStatus(200)
-			.expectBodyContains("game_id")
-			.afterJSON(function (){
-				frisby.create("Creating a conflicting game - 1")
-					.post(testHelper.createGameEndpoint, testHelper.createUnrestrictedGame(body.token, 100, 200))
-					.expectStatus(400)
-					.expectJSON({
-						error: strings.invalidGameScheduleConflict
+	it("Should not allow a user to create multiple games that conflict", function() {
+		return frisby.post(testHelper.registerEndpoint, testHelper.createGenericUserMale()) 
+			.expect("status", 200)
+			.expect("bodyContains", "token")
+			.expect("bodyContains", "user_id")
+			.expect("bodyContains", "refresh")
+			.then(function (body) {
+				body = body.json;
+				return frisby.post(testHelper.createGameEndpoint, testHelper.createUnrestrictedGame(body.token, 100, 200))
+					.expect("status", 200)
+					.expect("bodyContains", "game_id")
+					.then(function (){
+						return frisby.post(testHelper.createGameEndpoint, testHelper.createUnrestrictedGame(body.token, 100, 200))
+							.expect("status", 400)
+							.expect("jsonStrict", {
+								error: strings.invalidGameScheduleConflict
+							});
 					})
-					.toss();
-			})
-			.afterJSON(function (){
-				frisby.create("Creating a conflicting game - 2")
-					.post(testHelper.createGameEndpoint, testHelper.createUnrestrictedGame(body.token, 90, 300))
-					.expectStatus(400)
-					.expectJSON({
-						error: strings.invalidGameScheduleConflict
+					.then(function (){
+						return frisby.post(testHelper.createGameEndpoint, testHelper.createUnrestrictedGame(body.token, 90, 300))
+							.expect("status", 400)
+							.expect("jsonStrict", {
+								error: strings.invalidGameScheduleConflict
+							});
 					})
-					.toss();
-			})
-			.afterJSON(function (){
-				frisby.create("Creating a conflicting game - 3")
-					.post(testHelper.createGameEndpoint, testHelper.createUnrestrictedGame(body.token, 140, 300))
-					.expectStatus(400)
-					.expectJSON({
-						error: strings.invalidGameScheduleConflict
+					.then(function (){
+						return frisby.post(testHelper.createGameEndpoint, testHelper.createUnrestrictedGame(body.token, 140, 300))
+							.expect("status", 400)
+							.expect("jsonStrict", {
+								error: strings.invalidGameScheduleConflict
+							});
 					})
-					.toss();
-			})
-			.afterJSON(function (){
-				frisby.create("Creating a conflicting game - 4")
-					.post(testHelper.createGameEndpoint, testHelper.createUnrestrictedGame(body.token, 180, 300))
-					.expectStatus(400)
-					.expectJSON({
-						error: strings.invalidGameScheduleConflict
+					.then(function (){
+						return frisby.post(testHelper.createGameEndpoint, testHelper.createUnrestrictedGame(body.token, 180, 300))
+							.expect("status", 400)
+							.expect("jsonStrict", {
+								error: strings.invalidGameScheduleConflict
+							});
 					})
-					.toss();
-			})
-			.afterJSON(function (){
-				frisby.create("Creating a good game - 1")
-					.post(testHelper.createGameEndpoint, testHelper.createUnrestrictedGame(body.token, 55000, 300))
-					.expectStatus(200)
-					.expectBodyContains("game_id")
-					.toss();
-			})
-			.afterJSON(function (){
-				frisby.create("Creating a good game - 2")
-					.post(testHelper.createGameEndpoint, testHelper.createUnrestrictedGame(body.token, 0, 20))
-					.expectStatus(200)
-					.expectBodyContains("game_id")
-					.toss();
-			})
-			.toss();
-	})
-	.toss();
+					.then(function (){
+						return frisby.post(testHelper.createGameEndpoint, testHelper.createUnrestrictedGame(body.token, 55000, 300))
+							.expect("status", 200)
+							.expect("bodyContains", "game_id");
+					})
+					.then(function (){
+						return frisby.post(testHelper.createGameEndpoint, testHelper.createUnrestrictedGame(body.token, 0, 20))
+							.expect("status", 200)
+							.expect("bodyContains", "game_id");
+					});
+			});
+	});
 
-/*
-* Get users API test
-* */
-
-// Getting users of a game with valid input and successful response
-frisby.create("Joining a game: Creating a user to create a game")
-	.post(testHelper.registerEndpoint, testHelper.createGenericUserFixedBirth())
-	.expectStatus(200)
-	.expectBodyContains("token")
-	.afterJSON(function (user) {
-		frisby.create("Creating a new game")
-			.post(testHelper.createGameEndpoint, testHelper.createUnrestrictedGame(user.token, 100, 200))
-			.expectStatus(200)
-			.expectBodyContains("game_id")
-			.afterJSON(function (game) {
-				var gameId = game.game_id;
-				frisby.create("Creating a new user to join the game")
-					.post(testHelper.registerEndpoint, testHelper.createGenericUserFixedBirth())
-					.expectStatus(200)
-					.afterJSON(function (newUser) {
-						frisby.create("Join a game using the API with valid credentials")
-							.put(util.format(testHelper.joinGameEndpoint, gameId, newUser.token), newUser.token)
-							.expectStatus(200)
-							.expectBodyContains("game_id")
-							.afterJSON(function () {
-								frisby.create("Creating a new user to join the game")
-									.post(testHelper.registerEndpoint, testHelper.createGenericUserFixedBirth())
-									.expectStatus(200)
-									.afterJSON(function (newUser1) {
-										frisby.create("Join a game using the API with valid credentials")
-											.put(util.format(testHelper.joinGameEndpoint, gameId, newUser1.token), newUser1.token)
-											.expectStatus(200)
-											.expectBodyContains("game_id")
-											.afterJSON(function () {
-												frisby.create("Get the users of the game")
-													.get(testHelper.getUsersOfGameEndpoint + "?jwt=" + user.token + "&game_id=" + gameId)
-													.expectJSON("?", {
-														user_id: parseInt(newUser1.user_id),
-														reviewed: false,
-														username: newUser1.username
-													})
-													.expectJSON("?", {
-														user_id: parseInt(newUser.user_id),
-														reviewed: false,
-														username: newUser.username
-													})
-													.expectStatus(200)
-													.toss();
-											})
-											.toss();
-									})
-									.toss();
-							})
-							.toss();
-					})
-					.toss();
-			})
-			.toss();
-	})
-	.toss();
-/*
-* Join game API test
-* */
-
-// Joining game with valid input and successful response
-frisby.create("Joining a game: Creating a user to create a game")
-	.post(testHelper.registerEndpoint, testHelper.createGenericUserFixedBirth())
-	.expectStatus(200)
-	.expectBodyContains("token")
-	.afterJSON(function (user) {
-		frisby.create("Creating a new game")
-			.post(testHelper.createGameEndpoint, testHelper.createUnrestrictedGame(user.token, 100, 200))
-			.expectStatus(200)
-			.expectBodyContains("game_id")
-			.afterJSON(function (game) {
-				var gameId = game.game_id;
-				frisby.create("Creating a new user to join the game")
-					.post(testHelper.registerEndpoint, testHelper.createGenericUserFixedBirth())
-					.expectStatus(200)
-					.afterJSON(function (newUser) {
-						frisby.create("Join a game using the API with valid credentials")
-							.put(util.format(testHelper.joinGameEndpoint, gameId, newUser.token), newUser.token)
-							.expectStatus(200)
-							.expectBodyContains("game_id")
-							.toss();
-					})
-					.toss();
-			})
-			.toss();
-	})
-	.toss();
+	
+	it("Should be able to get the users of a game given the game id and a valid jwt", function() {
+		return frisby.post(testHelper.registerEndpoint, testHelper.createGenericUserFixedBirth())
+			.expect("status", 200)
+			.expect("bodyContains", "token")
+			.then(function (user) {
+				user = user.json;
+				return frisby.post(testHelper.createGameEndpoint, testHelper.createUnrestrictedGame(user.token, 100, 200))
+					.expect("status", 200)
+					.expect("bodyContains", "game_id")
+					.then(function (game) {
+						game = game.json;
+						var gameId = game.game_id;
+						var userDetails = testHelper.createGenericUserFixedBirth();
+						return frisby.post(testHelper.registerEndpoint, userDetails)
+							.expect("status", 200)
+							.then(function (newUser1) {
+								newUser1 = newUser1.json;
+								return frisby.put(util.format(testHelper.joinGameEndpoint, gameId, newUser1.token))
+									.expect("status", 200)
+									.expect("bodyContains", "game_id")
+									.then(function () {
+										var userDetails2 = testHelper.createGenericUserFixedBirth();
+										return frisby.post(testHelper.registerEndpoint, userDetails2)
+											.expect("status", 200)
+											.then(function (newUser2) {
+												newUser2 = newUser2.json;
+												return frisby.put(util.format(testHelper.joinGameEndpoint, gameId, newUser2.token))
+													.expect("status", 200)
+													.expect("bodyContains", "game_id")
+													.then(function () {
+														return frisby.get(testHelper.getUsersOfGameEndpoint + "?jwt=" + user.token + "&game_id=" + gameId)
+															.expect("jsonStrict", "?", {
+																user_id: newUser2.user_id,
+																reviewed: false,
+																username: userDetails2.username
+															})
+															.expect("jsonStrict", "?", {
+																user_id: newUser1.user_id,
+																reviewed: false,
+																username: userDetails.username
+															})
+															.expect("status", 200);
+													});
+											});
+									});
+							});
+					});
+			});
+	});
 
 
-/*
-* Leave game API tests
-* */
+	it("Should allow users the ability to join games", function() {
+		return frisby.post(testHelper.registerEndpoint, testHelper.createGenericUserFixedBirth())
+			.expect("status", 200)
+			.expect("bodyContains", "token")
+			.then(function (user) {
+				user = user.json;
+				return frisby.post(testHelper.createGameEndpoint, testHelper.createUnrestrictedGame(user.token, 100, 200))
+					.expect("status", 200)
+					.expect("bodyContains", "game_id")
+					.then(function (game) {
+						game = game.json;
+						var gameId = game.game_id;
+						return frisby.post(testHelper.registerEndpoint, testHelper.createGenericUserFixedBirth())
+							.expect("status", 200)
+							.then(function (newUser) {
+								newUser = newUser.json;
+								return frisby.put(util.format(testHelper.joinGameEndpoint, gameId, newUser.token))
+									.expect("status", 200)
+									.expect("bodyContains", "game_id");
+							});
+					});
+			});
+	});
 
-// Joining and leaving game. Valid response expected
-frisby.create("Joining a game: Creating a user to create a game")
-	.post(testHelper.registerEndpoint, testHelper.createGenericUserFixedBirth())
-	.expectStatus(200)
-	.expectBodyContains("token")
-	.afterJSON(function (user) {
-		frisby.create("Creating a new game")
-			.post(testHelper.createGameEndpoint, testHelper.createUnrestrictedGame(user.token, 100, 200))
-			.expectStatus(200)
-			.expectBodyContains("game_id")
-			.afterJSON(function (game) {
-				var gameId = game.game_id;
-				frisby.create("Creating a new user to join the game")
-					.post(testHelper.registerEndpoint, testHelper.createGenericUserFixedBirth())
-					.expectStatus(200)
-					.afterJSON(function (newUser) {
-						frisby.create("Join a game using the API with valid credentials")
-							.put(util.format(testHelper.joinGameEndpoint, gameId, newUser.token), newUser.token)
-							.expectStatus(200)
-							.expectBodyContains("game_id")
-							.afterJSON(function (joinedGame) {
-								frisby.create("Leaving a game using API - valid case")
-									.delete(util.format(testHelper.leaveGameEndpoint, joinedGame.game_id, newUser.token), newUser.token)
-									.expectStatus(200)
-									.toss();
-							})
-							.toss();
-					})
-					.toss();
-			})
-			.toss();
-	})
-	.toss();
 
-// Leaving a game the user did not join to begin with
-frisby.create("Joining a game: Creating a user to create a game")
-	.post(testHelper.registerEndpoint, testHelper.createGenericUserFixedBirth())
-	.expectStatus(200)
-	.expectBodyContains("token")
-	.afterJSON(function (user) {
-		frisby.create("Creating a new game")
-			.post(testHelper.createGameEndpoint, testHelper.createUnrestrictedGame(user.token, 100, 200))
-			.expectStatus(200)
-			.expectBodyContains("game_id")
-			.afterJSON(function (game) {
-				frisby.create("Creating a new user that will not join the game")
-					.post(testHelper.registerEndpoint, testHelper.createGenericUserFixedBirth())
-					.expectStatus(200)
-					.afterJSON(function (newUser) {
-						frisby.create("Leaving a game using API - user tries to leave a game the user has not joined")
-							.delete(util.format(testHelper.leaveGameEndpoint, game.game_id, newUser.token), newUser.token)
-							.expectStatus(400)
-							.toss();
-					})
-					.toss();
-			})
-			.toss();
-	})
-	.toss();
+	it("Should allow users to leave a game they have joined", function() {
+		return frisby.post(testHelper.registerEndpoint, testHelper.createGenericUserFixedBirth())
+			.expect("status", 200)
+			.expect("bodyContains", "token")
+			.then(function (user) {
+				user = user.json;
+				return frisby.post(testHelper.createGameEndpoint, testHelper.createUnrestrictedGame(user.token, 100, 200))
+					.expect("status", 200)
+					.expect("bodyContains", "game_id")
+					.then(function (game) {
+						game = game.json;
+						var gameId = game.game_id;
+						return frisby.post(testHelper.registerEndpoint, testHelper.createGenericUserFixedBirth())
+							.expect("status", 200)
+							.then(function (newUser) {
+								newUser = newUser.json;
+								return frisby.put(util.format(testHelper.joinGameEndpoint, gameId, newUser.token))
+									.expect("status", 200)
+									.expect("bodyContains", "game_id")
+									.then(function (joinedGame) {
+										joinedGame = joinedGame.json;
+										return frisby.del(util.format(testHelper.leaveGameEndpoint, joinedGame.game_id, newUser.token))
+											.expect("status", 200);
+									});
+							});
+					});
+			});
+	});
+
+
+	it("Should report a failure when a user tries to leave a game they have not joined", function() {
+		return frisby.post(testHelper.registerEndpoint, testHelper.createGenericUserFixedBirth())
+			.expect("status", 200)
+			.expect("bodyContains", "token")
+			.then(function (user) {
+				user = user.json;
+				return frisby.post(testHelper.createGameEndpoint, testHelper.createUnrestrictedGame(user.token, 100, 200))
+					.expect("status", 200)
+					.expect("bodyContains", "game_id")
+					.then(function (game) {
+						game = game.json;
+						return frisby.post(testHelper.registerEndpoint, testHelper.createGenericUserFixedBirth())
+							.expect("status", 200)
+							.then(function (newUser) {
+								newUser = newUser.json;
+								return frisby.del(util.format(testHelper.leaveGameEndpoint, game.game_id, newUser.token), newUser.token)
+									.expect("status", 400);
+							});
+					});
+			});
+	});
+
+});
