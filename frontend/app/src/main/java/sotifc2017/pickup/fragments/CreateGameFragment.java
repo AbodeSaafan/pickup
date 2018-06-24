93ee4e0f9dde6ae73ef81e842d70148db363a7b1
Abdulqader Saafan <abdulqader.saafan@mail.utoronto.ca>
2018-06-24 19:49:15 -0400

    #138: Improving game UI

--- frontend/app/src/main/java/sotifc2017/pickup/fragments/CreateGameFragment.java
+++ frontend/app/src/main/java/sotifc2017/pickup/fragments/CreateGameFragment.java
@@ -73,11 +73,6 @@ public class CreateGameFragment extends Fragment implements GetJwt.Callback {
 
     private GameModel gameModel;
 
-    private ImageButton detailsHeaderToggle;
-    private RelativeLayout gameDetailsSection;
-    private ImageButton restrictionsHeaderToggle;
-    private RelativeLayout gameRestrictionsSection;
-
     private EditText gameName;
     private EditText gameDescription;
 
@@ -92,7 +87,7 @@ public class CreateGameFragment extends Fragment implements GetJwt.Callback {
     private Calendar gameCalendar;
 
     private final String dateFormat = "MM/dd/yyyy";
-    private final String timeFormat = "HH:mm:ss";
+    private final String timeFormat = "HH:mm";
     Date startDefaultDate = new Date(System.currentTimeMillis());
     Date endDefaultDate = new Date(System.currentTimeMillis() + 360000);
     private EditText dateRangeText;
@@ -150,58 +145,6 @@ public class CreateGameFragment extends Fragment implements GetJwt.Callback {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         gameModel = new GameModel();
-
-        detailsHeaderToggle = view.findViewById(R.id.details_header_toggle);
-        gameDetailsSection = view.findViewById(R.id.game_details_section);
-        detailsHeaderToggle.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (gameDetailsSection.isShown()) {
-                    detailsHeaderToggle.setImageDrawable(ContextCompat.getDrawable(getActivity(), R.drawable.ic_expand_more_black_24dp));
-                    gameDetailsSection.animate().translationY(-1 * gameDetailsSection.getHeight()).setDuration(300).alpha(0.0f).withEndAction(new Runnable() {
-                        @Override
-                        public void run() {
-                            gameDetailsSection.setVisibility(View.GONE);
-                        }
-                    });
-                }
-                else {
-                    detailsHeaderToggle.setImageDrawable(ContextCompat.getDrawable(getActivity(), R.drawable.ic_expand_less_black_24dp));
-                    gameDetailsSection.animate().translationY(0).setDuration(300).alpha(1.0f).withStartAction(new Runnable() {
-                        @Override
-                        public void run() {
-                            gameDetailsSection.setVisibility(View.VISIBLE);
-                        }
-                    });
-                }
-            }
-        });
-
-        restrictionsHeaderToggle = view.findViewById(R.id.restrictions_header_toggle);
-        gameRestrictionsSection = view.findViewById(R.id.game_restrictions_section);
-        restrictionsHeaderToggle.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (gameRestrictionsSection.isShown()) {
-                    restrictionsHeaderToggle.setImageDrawable(ContextCompat.getDrawable(getActivity(), R.drawable.ic_expand_more_black_24dp));
-                    gameRestrictionsSection.animate().translationY(-1 * gameRestrictionsSection.getHeight()).setDuration(300).alpha(0.0f).withEndAction(new Runnable() {
-                        @Override
-                        public void run() {
-                            gameRestrictionsSection.setVisibility(View.GONE);
-                        }
-                    });
-                }
-                else {
-                    restrictionsHeaderToggle.setImageDrawable(ContextCompat.getDrawable(getActivity(), R.drawable.ic_expand_less_black_24dp));
-                    gameRestrictionsSection.animate().translationY(0).setDuration(300).alpha(1.0f).withStartAction(new Runnable() {
-                        @Override
-                        public void run() {
-                            gameRestrictionsSection.setVisibility(View.VISIBLE);
-                        }
-                    });
-                }
-            }
-        });
 
         gameName = view.findViewById(R.id.complete_text_view_game_name);
 
